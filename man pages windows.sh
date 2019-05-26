f0d34989ac5bf2bced2b101b34c9b294a959282d
Jake Bolton <ninmonkeys@gmail.com>
2019-05-26 02:57:11 -0500

    incrementally testing and learning bash

--- man pages windows.sh
+++ man pages windows.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Goal:
 #
 #     windows man pages don't exist, so to fake it:
@@ -7,18 +9,50 @@
 #     <command>   : same as linux man
 #     --web       : will open man page on the internet for better quality
 #
-#
-# 1. print echo; echo arg
+# example on shell:
+#   $ display_manual awk # or
+#   $ man awk
+
+
+# confusion on uses, because
+
+# works:
+#   $ man cat
+    # prints cat --help | less
+
+    # $ source ./man\ pages\ windows.sh cat
+    # just says "done"
+
+    # $ source ./man\ pages\ windows.sh cat ; man cat
+    # works then prints "done"
+
+# fails:
+#   $ source ./man\ pages\ windows.sh cat
+#   done
+
+
+
 
 display_manual() {
-    "$1" --help | less;
+    echo 'hi'
+    echo "1 = $1";
+    echo '2 = $2';
+    # "$1" --help | less;
+    "$1 --help"
+
+    echo 'bye'
 }
 display_manual_web() {
     echo 'nyi'
 }
 
 
+# $1 = 'less'
+# display_manual $1
+# display_manual_web
 
 alias man="display_manual"
 alias manw="display_manual_web"
 echo 'done';
+
+# display_manual 'zip'
